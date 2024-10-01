4af22388db9c7c059770675643e5ff6e9f841543
Disease <83523587+CPScript@users.noreply.github.com>
2024-10-01 01:45:15 -0400

    Update fs.h

--- include/fs.h
+++ include/fs.h
@@ -1,7 +1,23 @@
+// fs.h
 #ifndef FS_H
 #define FS_H
 
-void init_file_system();
-void read_file(const char *filename);
+#include <stdbool.h>
 
-#endif
+#define MAX_FILES 100
+#define FILENAME_LENGTH 50
+
+typedef struct {
+    char name[FILENAME_LENGTH];
+    bool is_directory; // Flag to indicate if it's a directory
+    // will add more file attributes later (like permissions, size, etc.)
+} File;
+
+File file_list[MAX_FILES]; // Array to store files
+int file_count = 0;         // Count of registered files
+
+bool create_file(const char *name, bool is_directory);
+bool delete_file(const char *name);
+void list_files();
+
+#endif // FS_H
