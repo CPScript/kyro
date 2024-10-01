d6dba709e287f6486b301dbaf3fdf6858b7df265
Disease <83523587+CPScript@users.noreply.github.com>
2024-10-01 02:12:43 -0400

    Update terminal.c

--- kernel/terminal.c
+++ kernel/terminal.c
@@ -3,17 +3,16 @@
 #include <stdio.h>
 #include <string.h>
 
-// Function to process commands
 void process_command(const char *command) {
     if (strncmp(command, "adduser ", 8) == 0) {
         char username[USERNAME_LENGTH];
         char password[PASSWORD_LENGTH];
-        sscanf(command + 8, "%s %s", username, password);
+        sscanf(command + 8, "%11s %11s", username, password);
         add_user(username, password, false); // Regular user
     } else if (strncmp(command, "login ", 6) == 0) {
         char username[USERNAME_LENGTH];
         char password[PASSWORD_LENGTH];
-        sscanf(command + 6, "%s %s", username, password);
+        sscanf(command + 6, "%11s %11s", username, password);
         if (authenticate(username, password)) {
             printf("Login successful!\n");
         } else {
@@ -21,23 +20,23 @@ void process_command(const char *command) {
         }
     } else if (strncmp(command, "create ", 7) == 0) {
         char filename[FILENAME_LENGTH];
-        sscanf(command + 7, "%s", filename);
+        sscanf(command + 7, "%49s", filename);
         create_file(filename);
     } else if (strncmp(command, "mkdir ", 6) == 0) {
         char dirname[FILENAME_LENGTH];
-        sscanf(command + 6, "%s", dirname);
+        sscanf(command + 6, "%49s", dirname);
         create_directory(dirname);
     } else if (strncmp(command, "rmdir ", 6) == 0) {
         char dirname[FILENAME_LENGTH];
-        sscanf(command + 6, "%s", dirname);
+        sscanf(command + 6, "%49s", dirname);
         delete_directory(dirname);
     } else if (strncmp(command, "delete ", 7) == 0) {
         char filename[FILENAME_LENGTH];
-        sscanf(command + 7, "%s", filename);
+        sscanf(command + 7, "%49s", filename);
         delete_file(filename);
     } else if (strncmp(command, "cat ", 4) == 0) {
         char filename[FILENAME_LENGTH];
-        sscanf(command + 4, "%s", filename);
+        sscanf(command + 4, "%49s", filename);
         read_file(filename);
     } else if (strcmp(command, "ls") == 0) {
         list_files();
