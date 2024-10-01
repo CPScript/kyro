9b74be7112313f808f32275bb91187029436c8ee
Disease <83523587+CPScript@users.noreply.github.com>
2024-10-01 01:16:10 -0400

    Create boot.asm

---
+++ boot/boot.asm
@@ -0,0 +1,19 @@
+; boot.asm
+section .text
+    global _start
+
+_start:
+    mov ah, 0x0E
+    mov si, message
+.next_char:
+    lodsb
+    cmp al, 0
+    je .done
+    int 0x10
+    jmp .next_char
+.done:
+    mov ax, 0x1000   ; Load kernel at 0x1000
+    jmp ax
+
+section .data
+message db 'Booting...', 0
