dda9eda058dbfaaaed3414f235e27b1397177441
Disease <83523587+CPScript@users.noreply.github.com>
2024-10-01 01:44:04 -0400

    Update user.h

--- include/user.h
+++ include/user.h
@@ -1,7 +1,22 @@
 #ifndef USER_H
 #define USER_H
 
-void init_users();
-void login_prompt();
+#include <stdbool.h>
+
+#define MAX_USERS 10
+#define USERNAME_LENGTH 50
+#define PASSWORD_LENGTH 50
+
+typedef struct {
+    char username[USERNAME_LENGTH];
+    char password[PASSWORD_LENGTH];
+    bool is_admin; // Flag for user roles
+} User;
+
+User user_list[MAX_USERS]; // Array to store users
+int user_count = 0;         // Count of registered users
+
+bool add_user(const char *username, const char *password, bool is_admin);
+bool authenticate(const char *username, const char *password);
 
 #endif
