1e7be7af5ba0a4ca24d64147ae67e80a5a60e8c0
Disease <83523587+CPScript@users.noreply.github.com>
2024-10-01 11:53:18 -0400

    Update user.c

--- kernel/user.c
+++ kernel/user.c
@@ -2,17 +2,26 @@
 #include <string.h>
 #include <stdio.h>
 
+unsigned long hash_password(const char *password) {
+    unsigned long hash = 5381;
+    int c;
+    while ((c = *password++)) {
+        hash = ((hash << 5) + hash) + c; // hash * 33 + c
+    }
+    return hash;
+}
+
 bool add_user(const char *username, const char *password, bool is_admin) {
     if (user_count >= MAX_USERS) {
         printf("User limit reached.\n");
         return false;
     }
-    if (strlen(username) >= USERNAME_LENGTH || strlen(password) >= PASSWORD_LENGTH) {
-        printf("Username or password too long.\n");
+    if (strlen(username) >= USERNAME_LENGTH) {
+        printf("Username too long.\n");
         return false;
     }
     strcpy(user_list[user_count].username, username);
-    strcpy(user_list[user_count].password, password); // need to consider hashing in the future
+    user_list[user_count].password_hash = hash_password(password);
     user_list[user_count].is_admin = is_admin;
     user_count++;
     return true;
@@ -21,7 +30,7 @@ bool add_user(const char *username, const char *password, bool is_admin) {
 bool authenticate(const char *username, const char *password) {
     for (int i = 0; i < user_count; i++) {
         if (strcmp(user_list[i].username, username) == 0 &&
-            strcmp(user_list[i].password, password) == 0) {
+            user_list[i].password_hash == hash_password(password)) {
             return true; // Successful authentication
         }
     }
