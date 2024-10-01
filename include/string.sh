bdcaf121d7cdc9adc92d28b194da56c86bcfca25
Disease <83523587+CPScript@users.noreply.github.com>
2024-10-01 14:50:06 -0400

    Create string.sh

---
+++ include/string.sh
@@ -0,0 +1,11 @@
+#ifndef STRING_H
+#define STRING_H
+
+#include <stdbool.h>
+
+char *strcpy(char *dest, const char *src);
+char *strcat(char *dest, const char *src);
+int strcmp(const char *s1, const char *s2);
+int strlen(const char *s);
+
+#endif
