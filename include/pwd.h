c85517d324f53cadbcd49c9b94db447e553aa8aa
Disease <83523587+CPScript@users.noreply.github.com>
2024-10-01 14:45:03 -0400

    Create pwd.h

---
+++ include/pwd.h
@@ -0,0 +1,16 @@
+#ifndef PWD_H
+#define PWD_H
+
+#include <stdbool.h>
+
+struct passwd {
+    char *pw_name;
+    char *pw_passwd;
+    uid_t pw_uid;
+    gid_t pw_gid;
+    char *pw_gecos;
+    char *pw_dir;
+    char *pw_shell;
+};
+
+#endif
