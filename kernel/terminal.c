4a56c0158383584336b7c6b49698eb0ced513d7a
Disease <83523587+CPScript@users.noreply.github.com>
2024-10-01 11:55:53 -0400

    Update terminal.c

--- kernel/terminal.c
+++ kernel/terminal.c
@@ -3,25 +3,37 @@
 #include <stdio.h>
 #include <string.h>
 
+void process_adduser_command(const char *command) {
+    char username[USERNAME_LENGTH];
+    char password[PASSWORD_LENGTH];
+    sscanf(command + 8, "%11s %11s", username, password);
+    add_user(username, password, false); // Regular user
+}
+
+void process_login_command(const char *command) {
+    char username[USERNAME_LENGTH];
+    char password[PASSWORD_LENGTH];
+    sscanf(command + 6, "%11s %11s", username, password);
+    if (authenticate(username, password)) {
+        printf("Login successful!\n");
+    } else {
+        printf("Login failed.\n");
+    }
+}
+
+void process_create_command(const char *command) {
+    char filename[FILENAME_LENGTH];
+    sscanf(command + 7, "%49s", filename);
+    create_file(filename);
+}
+
 void process_command(const char *command) {
     if (strncmp(command, "adduser ", 8) == 0) {
-        char username[USERNAME_LENGTH];
-        char password[PASSWORD_LENGTH];
-        sscanf(command + 8, "%11s %11s", username, password);
-        add_user(username, password, false); // Regular user
+        process_adduser_command(command);
     } else if (strncmp(command, "login ", 6) == 0) {
-        char username[USERNAME_LENGTH];
-        char password[PASSWORD_LENGTH];
-        sscanf(command + 6, "%11s %11s", username, password);
-        if (authenticate(username, password)) {
-            printf("Login successful!\n");
-        } else {
-            printf("Login failed.\n");
-        }
+        process_login_command(command);
     } else if (strncmp(command, "create ", 7) == 0) {
-        char filename[FILENAME_LENGTH];
-        sscanf(command + 7, "%49s", filename);
-        create_file(filename);
+        process_create_command(command);
     } else if (strncmp(command, "mkdir ", 6) == 0) {
         char dirname[FILENAME_LENGTH];
         sscanf(command + 6, "%49s", dirname);
