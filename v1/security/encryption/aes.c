a50dbaba373d37b3d2b8b5e4aad1904be6767ec2
Disease <83523587+CPScript@users.noreply.github.com>
2024-10-02 10:35:42 -0400

    Update aes.c

--- v1/security/encryption/aes.c
+++ v1/security/encryption/aes.c
@@ -1,1 +1,13 @@
+// aes.c
+// simple encryption
 
+#include <openssl/aes.h>
+
+void encrypt_data(void *data, int size) {
+    // Set up the AES encryption algorithm
+    AES_KEY aes_key;
+    AES_set_encrypt_key("secret_key", 128, &aes_key);
+
+    // Encrypt the data
+    AES_encrypt(data, data, &aes_key, size);
+}
