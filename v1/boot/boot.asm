1046ea139e83a55d5ab99387ca9971eb43c083a2
Disease <83523587+CPScript@users.noreply.github.com>
2024-10-02 10:38:41 -0400

    Update boot.asm

--- v1/boot/boot.asm
+++ v1/boot/boot.asm
@@ -25,19 +25,28 @@ _start:
 
 load_kernel:
     ; Read the kernel from the USB drive
-    mov eax, 0x100000
-    mov ecx, 0x1000
-    mov edx, 0x1000
-    call read_usb
-
-    ; Return the address of the kernel
-    ret
-
-read_usb:
-    ; Read from the USB drive using the BIOS
     mov eax, 0x13
     mov ebx, 0x100000
     mov ecx, 0x1000
     mov edx, 0x1000
     int 0x13
+
+    ; Check if the read was successful
+    jc read_error
+
+    ; Return the address of the kernel
     ret
+
+read_error:
+    ; Print an error message
+    mov eax, 0x4
+    mov ebx, 0x100000
+    mov ecx, error_message
+    mov edx, error_message_len
+    int 0x10
+
+    ; Halt the system
+    hlt
+
+error_message db 'Error reading kernel from USB drive', 0
+error_message_len equ $ - error_message
